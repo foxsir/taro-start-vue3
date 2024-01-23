c8f5968505696c411ffeda1af7216573ea25a4f7
foxsir <contact@umbst.com>
2024-01-23 16:12:10 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="index">
+    <view className="text-[#acc855] text-[100px]">Hello world!</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { reactive, toRefs } from 'vue';
+</script>
+
+<style lang="scss">
+
+</style>
